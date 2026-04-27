arpit-saxena/yugabyte-db

commit eda482d8bb7464cca3f6d74102a7697407db7e27

[#28012] YSQL: Always send custom ParameterStatus packet from Postgres to CM
Summary:
Add a byte for custom flags at the end of YB ParameterStatus packet (type `'r'`). These flags will be used to tell connection manager what sort of GUC setting was done so it can take decisions based on that. This diff doesn't change any external functionality -- previously incorrect things are still incorrect. This is an internal change where we just use our new packet type for Postgres->ConnMgr communication.

Only one bit is being set and read from the flags: `YB_PARAM_STATUS_REPORT_ENABLED`. This is set for variables that have `GUC_REPORT` enabled. Currently we are using it to induce the same difference as was being done with 'r'/'S' packets. in other words we are using it to decide whether to forward the packet to external client or not.
Jira: DB-17630

Test Plan: Jenkins: all tests

Reviewers: skumar, rbarigidad, mkumar, asrinivasan, vikram.damle, #db-approvers

Reviewed By: skumar, vikram.damle, #db-approvers

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D45460

src/odyssey/sources/frontend.c
@@ -899,9 +899,12 @@ static od_frontend_status_t od_frontend_remote_server(od_relay_t *relay,
 	case KIWI_BE_ERROR_RESPONSE:
 		od_backend_error(server, "main", data, size);
 		break;
-	/* fallthrough */
-	case YB_CONN_MGR_PARAMETER_STATUS:
 	case KIWI_BE_PARAMETER_STATUS:
+		od_error(
+			&instance->logger, "main", client, server,
+			"Refusing to parse unexpected 'S' ParameterStatus message from Postgres");
+		break;
+	case YB_CONN_MGR_PARAMETER_STATUS:
 		rc = od_backend_update_parameter(server, "main", data, size, 0);
 		if (rc == -1)
 			return relay->error_read;

src/odyssey/cmake/FindPostgreSQL.cmake
@@ -9,7 +9,7 @@
 if("${POSTGRESQL_INCLUDE_DIR}" STREQUAL "" OR "${POSTGRESQL_INCLUDE_DIR}" STREQUAL "POSTGRESQL_INCLUDE_DIR-NOTFOUND") 
     find_path(
         POSTGRESQL_INCLUDE_DIR
-        NAMES common/base64.h common/saslprep.h common/scram-common.h common/sha2.h
+        NAMES common/base64.h common/saslprep.h common/scram-common.h common/sha2.h common/pg_yb_param_status_flags.h
         PATH_SUFFIXES PG_INCLUDE_SERVER
     )
     execute_process (

src/odyssey/sources/backend.c
@@ -220,6 +220,22 @@ static int yb_read_client_id_from_notice_pkt(od_client_t *client,
 	return 0;
 }
 
+static inline int yb_send_parameter_status(od_io_t *io, char *name,
+					   int name_len, char *value,
+					   int value_len)
+{
+	machine_msg_t *msg = kiwi_be_write_parameter_status(
+		NULL, name, name_len, value, value_len);
+	if (msg == NULL) {
+		return -1;
+	}
+	int rc = od_write(io, msg);
+	if (rc != 0) {
+		return -1;
+	}
+	return 0;
+}
+
 static inline int od_backend_startup(od_server_t *server,
 				     kiwi_params_t *route_params,
 				     od_client_t *client)
@@ -377,17 +393,21 @@ static inline int od_backend_startup(od_server_t *server,
 				return -1;
 			}
 			break;
-		/* fallthrough */
-		case YB_CONN_MGR_PARAMETER_STATUS:
-		case KIWI_BE_PARAMETER_STATUS: {
+		case KIWI_BE_PARAMETER_STATUS:
+			od_error(
+				&instance->logger, "startup", NULL, server,
+				"Did not expect ParameterStatus 'S' packet from Postgres, refusing to parse");
+			return -1;
+		case YB_CONN_MGR_PARAMETER_STATUS: {
 			char *name;
 			uint32_t name_len;
 			char *value;
 			uint32_t value_len;
-			rc = kiwi_fe_read_parameter(machine_msg_data(msg),
-						    machine_msg_size(msg),
-						    &name, &name_len, &value,
-						    &value_len);
+			char flags;
+			rc = kiwi_fe_read_yb_parameter(machine_msg_data(msg),
+						       machine_msg_size(msg),
+						       &name, &name_len, &value,
+						       &value_len, &flags);
 			if (rc == -1) {
 				machine_msg_free(msg);
 				od_error(
@@ -397,12 +417,12 @@ static inline int od_backend_startup(od_server_t *server,
 				return -1;
 			}
 
-			if (is_authenticating)
-				od_debug(&instance->logger, "auth", NULL, server,
-			 			 "name: %s, value: %s", name, value);
-			else
-				od_debug(&instance->logger, "startup", NULL, server,
-			 			 "name: %s, value: %s", name, value);
+			od_debug(
+				&instance->logger,
+				is_authenticating ? "auth" : "startup", NULL,
+				server,
+				"Received YbParameterStatus, name: %.*s, value: %.*s, flags: 0x%X",
+				name_len, name, value_len, value, flags);
 
 			/* Parse the yb_logical_client_version to store it in server */
 			if (strlen(name) == 25 && strcmp("yb_logical_client_version", name) == 0) {
@@ -443,6 +463,11 @@ static inline int od_backend_startup(od_server_t *server,
 				yb_kiwi_vars_set_if_not_exists(
 					&client->yb_external_client->vars, name,
 					name_len, value, value_len);
+
+				/*
+				 * TODO(arpit.saxena, #27723): Send ParameterStatus directly here after we
+				 * start forwarding startup packet to authentication backend
+				 */
 			} else if ((name_len != sizeof("session_authorization") ||
 				strncmp(name, "session_authorization", name_len))) {
 				// set server parameters, ignore startup session_authorization
@@ -924,14 +949,26 @@ int od_backend_update_parameter(od_server_t *server, char *context, char *data,
 	uint32_t name_len;
 	char *value;
 	uint32_t value_len;
+	char flags = 0;
 
 	int rc;
-	rc = kiwi_fe_read_parameter(data, size, &name, &name_len, &value,
-				    &value_len);
+	rc = kiwi_fe_read_yb_parameter(data, size, &name, &name_len, &value,
+				       &value_len, &flags);
 	if (rc == -1) {
 		od_error(&instance->logger, context, NULL, server,
 			 "failed to parse ParameterStatus message");
 		return -1;
+	}
+
+	if (!server_only && flags & YB_PARAM_STATUS_REPORT_ENABLED) {
+		/* Send ParameterStatus to client if GUC_REPORT is enabled */
+		int rc = yb_send_parameter_status(&client->io, name, name_len,
+						  value, value_len);
+		if (rc != 0) {
+			od_error(&instance->logger, context, NULL, server,
+				 "Unable to send ParameterStatus to client");
+			return -1;
+		}
 	}
 
 	/* connection manager does not track role-dependent parameters */
@@ -975,7 +1012,13 @@ int od_backend_ready_wait(od_server_t *server, char *context, int count,
 		od_debug(&instance->logger, context, server->client, server,
 			 "%s", kiwi_be_type_to_string(type));
 
-		if (type == KIWI_BE_PARAMETER_STATUS || type == YB_CONN_MGR_PARAMETER_STATUS) {
+		if (type == KIWI_BE_PARAMETER_STATUS) {
+			od_error(
+				&instance->logger, context, server->client,
+				server,
+				"Unexpected ParameterStatus packet 'S' from postgres, refusing to parse");
+			continue;
+		} else if (type == YB_CONN_MGR_PARAMETER_STATUS) {
 			/* update server parameter */
 			int rc;
 			rc = od_backend_update_parameter(server, context,

src/odyssey/sources/postgres.h
@@ -35,4 +35,6 @@
 #include <common/hmac.h>
 #endif
 
+#include <common/pg_yb_param_status_flags.h>
+
 #endif /* ODYSSEY_POSTGRES_H */

src/odyssey/third_party/kiwi/kiwi/fe_read.h
@@ -152,6 +152,39 @@ kiwi_fe_read_parameter(char *data, uint32_t size, char **name,
 }
 
 KIWI_API static inline int
+kiwi_fe_read_yb_parameter(char *data, uint32_t size, char **name,
+			  uint32_t *name_len, char **value, uint32_t *value_len,
+			  char *flags)
+{
+	kiwi_header_t *header = (kiwi_header_t *)data;
+	uint32_t len;
+	int rc = kiwi_read(&len, &data, &size);
+	if (kiwi_unlikely(rc != 0))
+		return -1;
+	if (kiwi_unlikely(header->type != KIWI_BE_PARAMETER_STATUS) &&
+	    kiwi_unlikely(header->type != YB_CONN_MGR_PARAMETER_STATUS))
+		return -1;
+	uint32_t pos_size = len;
+	char *pos = kiwi_header_data(header);
+	/* name */
+	*name = pos;
+	rc = kiwi_readsz(&pos, &pos_size);
+	if (kiwi_unlikely(rc == -1))
+		return -1;
+	*name_len = pos - *name;
+	/* value */
+	*value = pos;
+	rc = kiwi_readsz(&pos, &pos_size);
+	if (kiwi_unlikely(rc == -1))
+		return -1;
+	*value_len = pos - *value;
+	rc = kiwi_read8(flags, &pos, &pos_size);
+	if (kiwi_unlikely(rc == -1))
+		return -1;
+	return 0;
+}
+
+KIWI_API static inline int
 kiwi_fe_read_error_or_notice(char *data, uint32_t size, kiwi_fe_error_t *error,
 			     kiwi_be_type_t expected_packet_type)
 {

src/postgres/src/backend/utils/misc/guc.c
@@ -119,6 +119,7 @@
 #include "access/heaptoast.h"
 #include "access/yb_scan.h"
 #include "commands/copy.h"
+#include "common/pg_yb_param_status_flags.h"
 #include "executor/ybModifyTable.h"
 #include "pg_yb_utils.h"
 #include "tcop/pquery.h"
@@ -8486,6 +8487,13 @@ SelectConfigFiles(const char *userDoption, const char *progname)
 	return true;
 }
 
+static bool
+yb_should_report_guc(struct config_generic *record)
+{
+	/* TODO(arpit.saxena): Use narrower sending criteria for correctness */
+	return ((record->flags & GUC_REPORT) && !YbIsClientYsqlConnMgr()) ||
+		(YbIsClientYsqlConnMgr() && record->context > PGC_BACKEND);
+}
 
 /*
  * Reset all options to their saved default values (implements RESET ALL)
@@ -8593,8 +8601,7 @@ ResetAllOptions(void)
 		gconf->scontext = gconf->reset_scontext;
 		gconf->srole = gconf->reset_srole;
 
-		if ((gconf->flags & GUC_REPORT && !YbIsClientYsqlConnMgr()) ||
-			(YbIsClientYsqlConnMgr() && gconf->context > PGC_BACKEND))
+		if (yb_should_report_guc(gconf))
 		{
 			gconf->status |= GUC_NEEDS_REPORT;
 			report_needed = true;
@@ -9016,9 +9023,7 @@ AtEOXact_GUC(bool isCommit, int nestLevel)
 			pfree(stack);
 
 			/* Report new value if we changed it */
-			if (changed &&
-				((gconf->flags & GUC_REPORT && !YbIsClientYsqlConnMgr()) ||
-				 (YbIsClientYsqlConnMgr() && gconf->context > PGC_BACKEND)))
+			if (changed && yb_should_report_guc(gconf))
 			{
 				gconf->status |= GUC_NEEDS_REPORT;
 				report_needed = true;
@@ -9146,27 +9151,28 @@ ReportGUCOption(struct config_generic *record)
 	{
 		StringInfoData msgbuf;
 
-		/*
-		 * YB: Do not bombard the client with ParameterStatus packets.
-		 * Send a specialized ParameterStatus packet to instruct Connection
-		 * Manager to not forward the packet to the client.
-		 *
-		 * 1. If GUC_REPORT is not enabled for the variable.
-		 * 2. If GUC_REPORT is enabled, but the previous value is the
-		 * same as the current value.
-		 */
-		bool		guc_report_not_enabled = !(record->flags & GUC_REPORT);
-		bool		guc_report_enabled_same_value = (record->flags & GUC_REPORT) &&
-			record->last_reported &&
-			strcmp(val, record->last_reported) == 0;
+		if (YbIsClientYsqlConnMgr())
+		{
+			uint8		flags = 0;
 
-		if (YbIsClientYsqlConnMgr() && (guc_report_not_enabled || guc_report_enabled_same_value))
+			if (record->flags & GUC_REPORT)
+				flags |= YB_PARAM_STATUS_REPORT_ENABLED;
+
+			/* TODO(arpit.saxena): Populate other bits in flags */
+
 			pq_beginmessage(&msgbuf, 'r');
+			pq_sendstring(&msgbuf, record->name);
+			pq_sendstring(&msgbuf, val);
+			pq_sendbyte(&msgbuf, flags);
+			pq_endmessage(&msgbuf);
+		}
 		else
+		{
 			pq_beginmessage(&msgbuf, 'S');
-		pq_sendstring(&msgbuf, record->name);
-		pq_sendstring(&msgbuf, val);
-		pq_endmessage(&msgbuf);
+			pq_sendstring(&msgbuf, record->name);
+			pq_sendstring(&msgbuf, val);
+			pq_endmessage(&msgbuf);
+		}
 
 		/*
 		 * We need a long-lifespan copy.  If strdup() fails due to OOM, we'll
@@ -10926,11 +10932,8 @@ set_config_option_ext(const char *name, const char *value,
 			}
 	}
 
-	if (changeVal &&
-		((record->flags & GUC_REPORT && !YbIsClientYsqlConnMgr()) ||
-		 (YbIsClientYsqlConnMgr() &&
-		  record->context > PGC_BACKEND &&
-		  !(action & GUC_ACTION_LOCAL))))
+	if (changeVal && yb_should_report_guc(record) &&
+		!(YbIsClientYsqlConnMgr() && (action & GUC_ACTION_LOCAL)))
 	{
 		record->status |= GUC_NEEDS_REPORT;
 		report_needed = true;

commit af59b81e4b9be24b667bc1d6a614dfff19a576f9

[#28850] YSQL: Don't forward ParameterStatus to client during deploy
Connection Manager was forwarding ParameterStatus packets to the
external client when re-attaching a logical connection to a physical
backend (the 'deploy' phase). This was a regression introduced in
eda482d8bb7464cca3f6d74102a7697407db7e27 -- previously the 'r' vs 'S'
packet type distinction prevented this, but with the unified 'r'
packet (carrying YB_PARAM_STATUS_REPORT_ENABLED), CM started forwarding
these packets even during deploy.

The fix: when od_frontend_remote_server() processes a
YB_CONN_MGR_PARAMETER_STATUS while the server is still in the deploy
phase, treat it as server-only (server_only=1) so we keep
yb_vars_default / yb_vars_session in sync without forwarding the
packet to the external client. The ParameterStatus the client expects
was already sent earlier in the lifetime of the logical connection
(it originated on the client's side), so re-forwarding it during
deploy is spurious traffic.

Adds an integration test that uses a custom JDBC SocketFactory to
inspect the inbound byte stream and assert that no 'S' ParameterStatus
packets are sent to the client while CM is deploying GUC state to a
fresh physical backend.

Co-authored-by: Arpit Saxena <arpit.saxena2000@yahoo.in>

src/odyssey/sources/frontend.c
@@ -779,7 +779,18 @@ static od_frontend_status_t od_frontend_remote_server(od_relay_t *relay,
 			"Refusing to parse unexpected 'S' ParameterStatus message from Postgres");
 		break;
 	case YB_CONN_MGR_PARAMETER_STATUS:
-		rc = od_backend_update_parameter(server, "main", data, size, 0);
+		/*
+		 * YB: When we are in the deploy phase, we are attaching the
+		 * logical connection to a (potentially fresh) physical backend
+		 * and replaying GUCs to it. The values being reported back by
+		 * Postgres are already known to the external client (they came
+		 * from the client's logical connection state), so we must not
+		 * forward ParameterStatus to the client. We still need to keep
+		 * server-side bookkeeping (yb_vars_default / yb_vars_session)
+		 * in sync, hence we pass server_only=1.
+		 */
+		rc = od_backend_update_parameter(server, "main", data, size,
+						 is_deploy);
 		if (rc == -1)
 			return relay->error_read;
 		break;

java/yb-ysql-conn-mgr/src/test/java/org/yb/ysqlconnmgr/CountParameterStatusSocketFactory.java
@@ -1,0 +1,235 @@
+// Copyright (c) YugabyteDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+package org.yb.ysqlconnmgr;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.net.SocketFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A SocketFactory implementation that creates sockets which inspect the inbound PostgreSQL
+ * protocol byte stream and count {@code ParameterStatus} ('S') packets received from the server.
+ *
+ * The count for the most recently created socket can be read via {@link #getLatestCount()}.
+ * The count can be reset to 0 via {@link #resetLatestCount()} so callers can measure counts for
+ * specific intervals during a connection's lifetime.
+ *
+ * This is used to verify that connection manager does not forward {@code ParameterStatus} packets
+ * to the external client when deploying a logical connection onto a physical backend.
+ */
+public class CountParameterStatusSocketFactory extends SocketFactory {
+  private static final Logger LOG =
+      LoggerFactory.getLogger(CountParameterStatusSocketFactory.class);
+
+  // PostgreSQL ParameterStatus message type byte.
+  private static final byte PARAMETER_STATUS = 'S';
+
+  // Tracks the count of ParameterStatus packets observed on the most recently created socket.
+  private static final AtomicInteger LATEST_COUNT = new AtomicInteger(0);
+
+  private final SocketFactory defaultFactory = SocketFactory.getDefault();
+
+  public CountParameterStatusSocketFactory() {
+    super();
+  }
+
+  public static int getLatestCount() {
+    return LATEST_COUNT.get();
+  }
+
+  public static void resetLatestCount() {
+    LATEST_COUNT.set(0);
+  }
+
+  @Override
+  public Socket createSocket() throws IOException {
+    return new InspectingSocket();
+  }
+
+  @Override
+  public Socket createSocket(String host, int port) throws IOException {
+    return new InspectingSocket(host, port);
+  }
+
+  @Override
+  public Socket createSocket(String host, int port, InetAddress localHost, int localPort)
+      throws IOException {
+    return new InspectingSocket(host, port, localHost, localPort);
+  }
+
+  @Override
+  public Socket createSocket(InetAddress host, int port) throws IOException {
+    return new InspectingSocket(host, port);
+  }
+
+  @Override
+  public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort)
+      throws IOException {
+    return new InspectingSocket(address, port, localAddress, localPort);
+  }
+
+  /**
+   * A socket whose input stream is a {@link CountingInputStream} that increments the
+   * ParameterStatus count whenever it sees an 'S' packet header in the byte stream.
+   */
+  private static class InspectingSocket extends Socket {
+    private CountingInputStream inputStream = null;
+
+    public InspectingSocket() throws IOException {
+      super();
+    }
+
+    public InspectingSocket(String host, int port) throws IOException {
+      super(host, port);
+    }
+
+    public InspectingSocket(String host, int port, InetAddress localHost, int localPort)
+        throws IOException {
+      super(host, port, localHost, localPort);
+    }
+
+    public InspectingSocket(InetAddress host, int port) throws IOException {
+      super(host, port);
+    }
+
+    public InspectingSocket(
+        InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+      super(address, port, localAddress, localPort);
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+      if (inputStream == null) {
+        inputStream = new CountingInputStream(super.getInputStream());
+      }
+      return inputStream;
+    }
+
+    @Override
+    public OutputStream getOutputStream() throws IOException {
+      return super.getOutputStream();
+    }
+  }
+
+  /**
+   * An InputStream that walks the Postgres v3 protocol byte stream and counts every
+   * {@code ParameterStatus} ('S') message. We rely on the fact that all v3 messages from the
+   * server start with a 1-byte type, followed by a 4-byte big-endian length (which includes
+   * itself).
+   */
+  private static class CountingInputStream extends InputStream {
+    private final InputStream delegate;
+
+    // Parser state: how many more body bytes (including the 4-byte length field) of the current
+    // message we still need to consume before we expect the next message-type byte.
+    private long remainingBodyBytes = 0;
+    // While reading the 4-byte length field, this holds bytes seen so far (0..4).
+    private int lengthBytesSeen = 0;
+    private long pendingLength = 0;
+    private boolean awaitingType = true;
+
+    public CountingInputStream(InputStream delegate) {
+      this.delegate = delegate;
+    }
+
+    private void processByte(byte b) {
+      if (awaitingType) {
+        if (b == PARAMETER_STATUS) {
+          int count = LATEST_COUNT.incrementAndGet();
+          LOG.info("Observed ParameterStatus ('S') packet from server. Count = " + count);
+        }
+        awaitingType = false;
+        lengthBytesSeen = 0;
+        pendingLength = 0;
+        return;
+      }
+
+      if (lengthBytesSeen < 4) {
+        pendingLength = (pendingLength << 8) | (b & 0xFFL);
+        lengthBytesSeen++;
+        if (lengthBytesSeen == 4) {
+          // Length field includes itself (4 bytes), so subtract them to get remaining body bytes.
+          remainingBodyBytes = pendingLength - 4;
+          if (remainingBodyBytes <= 0) {
+            awaitingType = true;
+          }
+        }
+        return;
+      }
+
+      remainingBodyBytes--;
+      if (remainingBodyBytes <= 0) {
+        awaitingType = true;
+      }
+    }
+
+    @Override
+    public int read() throws IOException {
+      int b = delegate.read();
+      if (b != -1) {
+        processByte((byte) b);
+      }
+      return b;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+      return read(b, 0, b.length);
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+      int bytesRead = delegate.read(b, off, len);
+      if (bytesRead > 0) {
+        for (int i = 0; i < bytesRead; i++) {
+          processByte(b[off + i]);
+        }
+      }
+      return bytesRead;
+    }
+
+    @Override
+    public int available() throws IOException {
+      return delegate.available();
+    }
+
+    @Override
+    public void close() throws IOException {
+      delegate.close();
+    }
+
+    @Override
+    public synchronized void mark(int readlimit) {
+      delegate.mark(readlimit);
+    }
+
+    @Override
+    public synchronized void reset() throws IOException {
+      delegate.reset();
+    }
+
+    @Override
+    public boolean markSupported() {
+      return delegate.markSupported();
+    }
+  }
+}

java/yb-ysql-conn-mgr/src/test/java/org/yb/ysqlconnmgr/TestSessionParameters.java
@@ -27,6 +27,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Arrays;
@@ -835,6 +836,138 @@ public class TestSessionParameters extends BaseYsqlConnMgr {
       } catch (Exception e) {
         assertTrue("Expected error about parameter cannot be set after connection start",
             e.getMessage().contains("cannot be set after connection start"));
+      }
+    }
+  }
+
+  // GH #28850: Connection Manager must NOT forward ParameterStatus packets to the external client
+  // when it is deploying a logical connection onto a physical backend. The state being applied to
+  // the physical backend was originally set by the same logical connection, so it is already known
+  // to the client; sending a ParameterStatus during deploy is spurious traffic.
+  //
+  // To verify, we use a custom socket factory that counts inbound 'S' (ParameterStatus) packets.
+  // After establishing the logical connection and issuing SET statements, we reset the counter,
+  // then force the logical connection to attach to a different physical backend by occupying its
+  // current backend with another logical connection (in a transaction) and waking it up. We expect
+  // zero ParameterStatus packets to arrive at the client during this deploy.
+  @Test
+  public void testNoParameterStatusSentDuringDeploy() throws Exception {
+    final String socketFactoryClass =
+        "org.yb.ysqlconnmgr.CountParameterStatusSocketFactory";
+
+    // Test connection that will be migrated across physical backends. We attach the counting
+    // socket factory only to this connection, so any ParameterStatus we observe corresponds to
+    // its lifecycle.
+    Properties props = new Properties();
+    props.setProperty("user", "yugabyte");
+    props.setProperty("socketFactory", socketFactoryClass);
+
+    // Helper connections used to occupy / free the physical backends. These deliberately do not
+    // use the inspecting socket factory so we don't pollute the counter.
+    try (Connection mainConn = getConnectionBuilder()
+            .withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+            .connect(props);
+        Connection helper1 = getConnectionBuilder()
+            .withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+            .connect();
+        Connection helper2 = getConnectionBuilder()
+            .withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+            .connect();
+        Connection helper3 = isTestRunningInWarmupRandomMode() ? getConnectionBuilder()
+            .withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+            .connect() : null;
+        Statement mainStmt = mainConn.createStatement();
+        Statement helperStmt1 = helper1.createStatement();
+        Statement helperStmt2 = helper2.createStatement();
+        Statement helperStmt3 = (helper3 != null) ? helper3.createStatement() : null) {
+
+      // Issue SET statements that touch GUC_REPORT variables. These should produce
+      // ParameterStatus packets at this point in time (which we are not measuring).
+      mainStmt.execute("SET application_name = 'cm_deploy_test'");
+      mainStmt.execute("SET TimeZone = 'EST'");
+
+      // Confirm we received some ParameterStatus packets so far (so we know the counter works).
+      int countBeforeDeploy = CountParameterStatusSocketFactory.getLatestCount();
+      assertTrue("Expected at least one ParameterStatus packet during initial setup, got "
+              + countBeforeDeploy, countBeforeDeploy > 0);
+
+      // Find the backend PID we are currently attached to.
+      ResultSet rs = mainStmt.executeQuery("SELECT pg_backend_pid()");
+      assertTrue("backend pid should be non-null", rs.next());
+      int initialPID = rs.getInt(1);
+
+      // Saturate every other slot in the pool with a transaction so when we re-attach later, we
+      // cannot land back on the same physical backend.
+      helperStmt1.execute("BEGIN");
+      helperStmt1.execute("SELECT 1");
+      helperStmt2.execute("BEGIN");
+      helperStmt2.execute("SELECT 1");
+      if (helperStmt3 != null) {
+        helperStmt3.execute("BEGIN");
+        helperStmt3.execute("SELECT 1");
+      }
+
+      // Now reset the counter -- we are about to force a deploy on the main connection, and want
+      // to count only the packets received during that flow.
+      CountParameterStatusSocketFactory.resetLatestCount();
+
+      // Force the main connection to attach to a different physical backend by issuing a query
+      // after detaching from the current one. Since the pool has only `ysql_conn_mgr_max_conns_per_db`
+      // slots, all of which are either occupied by helpers' transactions or by mainConn itself,
+      // committing helperStmt1 frees a slot. Wrapping mainConn's next query in a fresh statement
+      // (transaction-pooled detach happens at end of last transaction) requires us to first detach
+      // mainConn. We do so by ensuring it is NOT in a transaction and issuing a single query.
+      // The simplest way to deterministically force re-attach is:
+      //   1. Have helperStmt1 commit (frees its physical backend slot)
+      //   2. Have helperStmt2 BEGIN a new transaction (ensures a slot is held by it)
+      //   3. Issue a query on mainConn -- it must attach to a different physical backend than its
+      //      original since helpers are still occupying others.
+      // To make this deterministic across pool sizes, we just iterate over a small number of
+      // queries that each cause an attach/detach cycle.
+      for (int i = 0; i < 5; i++) {
+        // Cycle helpers so the slot mainConn can attach to keeps changing.
+        helperStmt1.execute("COMMIT");
+        helperStmt1.execute("BEGIN");
+        helperStmt1.execute("SELECT 1");
+
+        ResultSet rs2 = mainStmt.executeQuery("SELECT pg_backend_pid()");
+        assertTrue("backend pid should be non-null", rs2.next());
+        int currentPID = rs2.getInt(1);
+        rs2.close();
+
+        // Confirm the GUC values are still correctly set on whatever backend we land on.
+        try (ResultSet rs3 = mainStmt.executeQuery("SHOW application_name")) {
+          assertTrue(rs3.next());
+          assertEquals("cm_deploy_test", rs3.getString(1));
+        }
+        try (ResultSet rs3 = mainStmt.executeQuery("SHOW TimeZone")) {
+          assertTrue(rs3.next());
+          assertEquals("EST", rs3.getString(1));
+        }
+
+        if (currentPID != initialPID) {
+          // We attached to a new physical backend at least once -- this means deploy ran.
+          break;
+        }
+      }
+
+      // The crucial assertion: during deploy (and any SHOW queries), we must NOT have received any
+      // ParameterStatus packets, because:
+      //   - SET statements were issued before resetLatestCount() was called.
+      //   - SHOW statements only return rows; they do not generate ParameterStatus packets.
+      //   - Deploy replays GUCs to the new physical backend, but those replays should NOT be
+      //     forwarded to the client.
+      int countAfterDeploy = CountParameterStatusSocketFactory.getLatestCount();
+      assertEquals(
+          "ConnMgr must not forward ParameterStatus packets to client during deploy. Got "
+              + countAfterDeploy + " ParameterStatus packets, expected 0.",
+          0, countAfterDeploy);
+
+      // Cleanup
+      helperStmt1.execute("COMMIT");
+      helperStmt2.execute("COMMIT");
+      if (helperStmt3 != null) {
+        helperStmt3.execute("COMMIT");
       }
     }
   }